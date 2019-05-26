71162857e5635377ab9afbd107e43413299a32e9
DiniyNifail <diniynifail92@hotmail.com>
2019-05-26 23:13:34 +0800

    Second Version with random number and import dart:math. Add in random number generator for the dice.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -22,7 +23,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -31,20 +39,26 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('left Button got pressed');
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+                print('Left dice number is $leftDiceNumber');
               },
               child: Image.asset(
-                'images/dice1.png',
+                'images/dice$leftDiceNumber.png',
               ),
             ),
           ),
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right Button got pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+                print('Right dice number is $rightDiceNumber');
               },
               child: Image.asset(
-                'images/dice1.png',
+                'images/dice$rightDiceNumber.png',
               ),
             ),
           ),
